a164109d668c063bed32cc0d27d0fae3b76d1d99
Michael <michaelthejames@gmail.com>
2026-07-29 10:57:17 -0500

    removed unused files and added a trigger to home assistant for each shot taken

--- test-savetohost.js
+++
@@ -1,8 +0,0 @@
-// test-savetohost.js
-const response = await fetch('https://localhost:3000/camera/0/SaveTo', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ value: 'Host' }),
-});
-console.log('Status:', response.status);
-console.log('Body:', await response.json());
